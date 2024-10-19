00505b5581b97c6bf85e97ee9329d3c613c07473
yuichkun <yuichkun@gmail.com>
2024-10-19 22:38:15 +0900

    add googletest

---
+++ test/source/AudioProcessorTest.cpp
@@ -0,0 +1,10 @@
+#include <gtest/gtest.h>
+#include <YogoAudioPlugin/PluginProcessor.h>
+
+namespace yogo_audio_plugin_test {
+TEST(AudioPluginAudioProcessor, Foo)
+{
+  AudioPluginAudioProcessor processor;
+  ASSERT_FALSE(false);
+}
+}
